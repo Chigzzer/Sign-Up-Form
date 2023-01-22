e8266f0cab682fff401dd731a6b4ff376776e707
Chigzzer <42282981+Chigzzer@users.noreply.github.com>
2023-01-22 18:08:13 +0000

    Added javascript pw check

--- script.js
+++ script.js
@@ -2,16 +2,19 @@ const password = document.querySelector("#password");
 const repeated_password = document.querySelector("#rpassword");
 const text = document.getElementById('pwmatch');
 let message;
+let borderColor;
 repeated_password.addEventListener('input', checkPW);
-
+let error = '\u{026A0}'; 
 
 
 function checkPW(){
     if (document.getElementById('rpassword').value == document.getElementById('password').value){
         message = "Your passwords match";
+        borderColor = '3px solid green';
     }
     else{
-        message = "Your passwords do not match";
+        message = error + "Your passwords do not match";
+        borderColor = '1px solid red';
     }
     if (!document.getElementById('rpassword').value.length){
         text.classList.add('hidden');
@@ -19,5 +22,7 @@ function checkPW(){
     else{
         text.innerText = message;
         text.classList.remove('hidden');
+        document.getElementById('password').style.border = borderColor;
+        document.getElementById('rpassword').style.border = borderColor;
     }
 }
