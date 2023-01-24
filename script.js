603420eec1cf5cc7ef42d069af1b94c0951a7b98
Chigzzer <42282981+Chigzzer@users.noreply.github.com>
2023-01-24 21:32:02 +0000

    Added password requirements

--- script.js
+++ script.js
@@ -4,23 +4,54 @@ const text = document.getElementById('pwmatch');
 let message;
 let borderColor;
 repeated_password.addEventListener('input', checkPW);
+password.addEventListener('input', checkPW);
 let error = '\u{026A0}'; 
 
 
 function checkPW(){
+    passwordMatch();
+    passwordLength();
+}
+
+function passwordLength(){
+    let nodeLength = document.querySelector('.requirements');
+    console.log(document.getElementById('password').value.length)
+    if (document.getElementById('password').value.length < 8){
+        nodeLength.classList.remove('lengthcorrect');
+        nodeLength.classList.add('lengthwrong');
+    }
+    else{
+        nodeLength.classList.remove('lengthwrong');
+        nodeLength.classList.add('lengthcorrect');
+    }
+
+}
+
+function passwordMatch(){
+    let matchingNodes = document.getElementsByClassName('matching');
     if (document.getElementById('rpassword').value == document.getElementById('password').value){
-        message = "Your passwords match";
+        while (matchingNodes[0]){
+            text.removeChild(matchingNodes[0]);
+        }
+        message = "Your passwords match \n";
         borderColor = '3px solid green';
     }
     else{
+        while (matchingNodes[0]){
+            text.removeChild(matchingNodes[0]);
+        }
         message = error + "Your passwords do not match";
-        borderColor = '1px solid red';
+        borderColor = '3px solid red';
     }
-    if (!document.getElementById('rpassword').value.length){
+
+    if (!document.getElementById('rpassword').value){
         text.classList.add('hidden');
     }
     else{
-        text.innerText = message;
+        let matchText = document.createElement('div');
+        matchText.innerText = message;
+        matchText.classList.add('matching');
+        text.appendChild(matchText);
         text.classList.remove('hidden');
         document.getElementById('password').style.border = borderColor;
         document.getElementById('rpassword').style.border = borderColor;
