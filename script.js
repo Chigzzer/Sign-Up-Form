0ef144e1986e9a4393508b3355a1ba8ef40f808c
Chigzzer <42282981+Chigzzer@users.noreply.github.com>
2023-11-23 22:30:56 +0000

    Updated requirements to variables for future use

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
 let passwordBox = document.querySelector('#password');
 let confirmPasswordBox = document.querySelector('#confirm-password');
 let pwRequirements = document.querySelector('#pwd-requirements');
-
+let passwordMatching;
+let passwordLength;
+let passwordCharacter;
+let passwordNumber;
+let passwordCorrect;
 
 
 confirmPasswordBox.addEventListener('input', checkPW);
@@ -26,15 +30,20 @@ function checkPW(){
     pwLength();
     pwUppercase();
     pwNumber();
+    passwordCorrect();
 }
 
 function passwordMatch(){
-    if (confirmPasswordBox.value == passwordBox.value){
+    if (confirmPasswordBox.value == passwordBox.value && confirmPasswordBox.value != ''){
         console.log('yes');
         document.querySelector('#pw-match-text').style.color = 'green';
+        document.querySelector('#pw-match-text').innerHTML = 'Passwords match';
+        passwordMatching = true;
     }
     else{
         document.querySelector('#pw-match-text').style.color = 'red';
+        document.querySelector('#pw-match-text').innerHTML = 'Passwords do not match';
+        passwordMatching = false;
     }
 }
 
@@ -42,9 +51,11 @@ function pwLength(){
     console.log(passwordBox.value.length)
     if (passwordBox.value.length < 8){
         document.querySelector('#pw-length-text').style.color = 'red';
+        passwordLength = false;
     }
     else{
         document.querySelector('#pw-length-text').style.color = 'green';
+        passwordLength = true;
     }
 }
 
@@ -54,17 +65,34 @@ function pwUppercase(){
 
     if (upper && lower){
         document.querySelector('#uppercase-text').style.color = 'green';
+        passwordCharacter = true;
     }
     else{
         document.querySelector('#uppercase-text').style.color = 'red';
+        passwordCharacter = false;
     }
 }
 
 function pwNumber(){
     if (/\d/.test(passwordBox.value)){
         document.querySelector('#number-text').style.color = 'green';
+        passwordNumber = true;
     }
     else{
         document.querySelector('#number-text').style.color = 'red';
+        passwordNumber = false;
+    }
+}
+
+function passwordCorrect(){
+    if ( passwordMatching && passwordCharacter && passwordLength && passwordNumber){
+        passwordBox.style.border = '1px solid green';
+        confirmedPasswordBox.style.border = '1px solid green';
+        passwordCorrect = true;
+    }
+    else{
+        passwordBox.style.border = '2px solid red';
+        confirmedPasswordBox.style.border = '2px solid red';
+        passwordCorrect = false;
     }
 }
