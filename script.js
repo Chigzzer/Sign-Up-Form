0ae80090f389c8ffef7d0e82ac387500bc691228
Chigzzer <42282981+Chigzzer@users.noreply.github.com>
2023-11-23 21:21:50 +0000

    Password requirements confirmation added

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ let pwRequirements = document.querySelector('#pwd-requirements');
 
 
 confirmPasswordBox.addEventListener('input', checkPW);
+passwordBox.addEventListener('input', checkPW);
 passwordBox.addEventListener('focus', showPwRequirements);
 passwordBox.addEventListener('blur', hidePwRequirements);
 confirmPasswordBox.addEventListener('focus', showPwRequirements);
@@ -39,6 +40,9 @@ function hidePwRequirements(){
 
 function checkPW(){
     passwordMatch();
+    pwLength();
+    pwUppercase();
+    pwNumber();
 }
 
 function passwordMatch(){
@@ -48,5 +52,36 @@ function passwordMatch(){
     }
     else{
         document.querySelector('#pw-match-text').style.color = 'red';
+    }
+}
+
+function pwLength(){
+    console.log(passwordBox.value.length)
+    if (passwordBox.value.length < 8){
+        document.querySelector('#pw-length-text').style.color = 'red';
+    }
+    else{
+        document.querySelector('#pw-length-text').style.color = 'green';
+    }
+}
+
+function pwUppercase(){
+    let upper = /[A-Z]/.test(passwordBox.value);
+    let lower = /[a-z]/.test(passwordBox.value);
+
+    if (upper && lower){
+        document.querySelector('#uppercase-text').style.color = 'green';
+    }
+    else{
+        document.querySelector('#uppercase-text').style.color = 'red';
+    }
+}
+
+function pwNumber(){
+    if (/\d/.test(passwordBox.value)){
+        document.querySelector('#number-text').style.color = 'green';
+    }
+    else{
+        document.querySelector('#number-text').style.color = 'red';
     }
 }
