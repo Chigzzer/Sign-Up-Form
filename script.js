fcb87c6e67d6d09349fafe25d0cce3536fd8163b
Chigzzer <42282981+Chigzzer@users.noreply.github.com>
2023-11-23 21:41:59 +0000

    Updated layout with requirements

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-let holidaySection = document.querySelector('#holiday-section');
 let passwordBox = document.querySelector('#password');
 let confirmPasswordBox = document.querySelector('#confirm-password');
 let pwRequirements = document.querySelector('#pwd-requirements');
@@ -12,30 +11,14 @@ passwordBox.addEventListener('blur', hidePwRequirements);
 confirmPasswordBox.addEventListener('focus', showPwRequirements);
 confirmPasswordBox.addEventListener('blur', hidePwRequirements);
 
-function holidaySelector(){
-    console.log('running');
-    let holidayKnown = document.querySelector('input[name="holidayQuery"]:checked').value;
-    console.log(holidayKnown);
-    if (holidayKnown == 'yes'){
-        console.log('YES');
-        holidaySection.classList.remove('hidden');
-    }
-    else{
-        console.log('NO');
-        document.querySelector('#location-text').value = '';
-        document.querySelector('#yearOfTravel').value = '';
-        holidaySection.classList.add('hidden');
-
-    }
-}
 
 
 function showPwRequirements(){
-    pwRequirements.classList.remove('none');
+    pwRequirements.classList.remove('hidden');
 }
 
 function hidePwRequirements(){
-    pwRequirements.classList.add('none');
+    pwRequirements.classList.add('hidden');
 }
 
 function checkPW(){
